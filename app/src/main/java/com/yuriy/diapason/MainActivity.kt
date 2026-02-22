43201135c859fcd904669f88f4e547a50f246acf
Yurii <chernyshov.yuriy@gmail.com>
2026-02-21 23:01:25 -0500

    Fix Screen ON

--- app/src/main/java/com/yuriy/diapason/MainActivity.kt
+++ app/src/main/java/com/yuriy/diapason/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.yuriy.diapason
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.yuriy.diapason.ui.theme.DiapasonTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,5 +17,15 @@ class MainActivity : ComponentActivity() {
                 DiapasonAppMainView()
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+    }
+
+    override fun onPause() {
+        window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        super.onPause()
     }
 }
